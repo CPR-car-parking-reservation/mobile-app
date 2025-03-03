79952f16f0d91c45f0cde66d73447a3718b16541
Ohmmy569 <Ohmmy569@gmail.com>
2025-03-04 02:46:05 +0700

    commit : fix duplicate line in input

--- lib/model/car.dart
+++ lib/model/car.dart
@@ -1,32 +1,31 @@
 class car_data {
-    String id;
-    String license_plate;
-    String car_model;
-    String car_type;
-    String user_id;
-    String image_url;
+  String id;
+  String license_plate;
+  String car_model;
+  String car_type;
+  String user_id;
+  String image_url;
 
-    car_data({
-        required this.id,
-        required this.license_plate,
-        required this.car_model,
-        required this.car_type,
-        required this.user_id,
-        required this.image_url,
-    });
+  car_data({
+    required this.id,
+    required this.license_plate,
+    required this.car_model,
+    required this.car_type,
+    required this.user_id,
+    required this.image_url,
+  });
 
-    factory car_data.fromJson(Map<String, dynamic> json) => car_data(
+  factory car_data.fromJson(Map<String, dynamic> json) => car_data(
         id: json["id"] ?? '',
         license_plate: json["license_plate"] ?? '',
         car_model: json["car_model"] ?? '',
         car_type: json["car_type"] ?? '',
         user_id: json["user_id"] ?? '',
         image_url: json["image_url"] ?? '',
-    );
+      );
 
   Map<String, dynamic> toJson() => {
         "id": id,
-        "license_plate": license_plate,
         "license_plate": license_plate,
         "car_model": car_model,
         "car_type": car_type,
